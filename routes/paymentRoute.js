ebbd84740ae4135747f64be3e3d25a68e4c4e193
Muhammad Junaid <86664791+MuhammadJunaid01@users.noreply.github.com>
2023-08-10 19:06:54 +0600

    try to fix cors policy:

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -10,6 +10,11 @@ const stripe = Stripe(process.env.STRIPE_SECRET);
 router.post("/payment", async (req, res, next) => {
   const { products, token, amount, user, status } = req.body;
   const idempentencyKey = uuidv4();
+  console.log("Request Headers:", req.headers); // Debugging line
+  //  Set CORS headers manually for this route
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   try {
     const order = new Order({
